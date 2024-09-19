bd552355aec26f9f15947bed67365eaa7315a6ca
Marianvsf <marianvsf@gmail.com>
2024-09-19 05:19:33 +0000

    firts

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,23 @@
+import React  from "react";
+//import Container from 'react-bootstrap/Container';
+
+export const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center" bg="dark" data-bs-theme="dark" 
+  style={{ background: 'black', color: 'grey'}} > 
+		<p>Made by 
+          <a href="https://github.com/Marianvsf"> Marian Suarez </a>
+          at
+          <a href="http://www.4geeksacademy.com"> 4GeeksAcademy 
+            <img className="logo"
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.NzJUc0PYtkgp7lfNKizmgQHaHB%26pid%3DApi&f=1&ipt=271cb3eb7e5f15522c250313a63e781bdb17fd428148a3cfa3fc136c7c118f42&ipo=images"
+              style={{
+                border: "1px solid rgb(255, 255, 255)",
+                borderRadius: "50%",
+                width: "35px",
+              }}
+              alt="logo"
+            />
+          </a>
+		</p>
+	</footer>
+);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
+import SimpleCounter from "./Count.jsx"
+import { Footer } from "./footer.jsx";
 
 //create your first component
 const Home = () => {
-	return (
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		let intervalId = setInterval(() => {
+			setCount(count +1)},
+		1000)
+	return () => 
+		clearInterval(intervalId)}
+},	 [count]
+return () => {
+
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div>
+				<SimpleCounter/>
+			</div>
+			<div className="fixed-bottom">
+				<Footer/>
+			</div>
 		</div>
-	);
-};
+}
+
+	
 
 export default Home;
